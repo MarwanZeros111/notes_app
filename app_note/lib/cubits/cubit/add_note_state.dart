78e1b8e9069a5cd5f52b22b9d386f75421021a83
Marwan1231231 <94732913+Marwan1231231@users.noreply.github.com>
2023-10-04 01:47:03 +0300

    add notes state

--- app_note/lib/cubits/cubit/add_note_state.dart
+++ app_note/lib/cubits/cubit/add_note_state.dart
@@ -3,4 +3,14 @@ import 'package:flutter/material.dart';
 @immutable
 abstract class NotesState {}
 
-class NotesInitial extends NotesState {}
+class AddNotesInitial extends NotesState {}
+
+class AddNotesLoading extends NotesState {}
+
+class AddNotesSuccess extends NotesState {}
+
+class AddNotesFaluir extends NotesState {
+  final String errMessage;
+
+  AddNotesFaluir(this.errMessage);
+}
