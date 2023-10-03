9d18233d0978cb519249c17916cffe8252155c89
Marwan1231231 <94732913+Marwan1231231@users.noreply.github.com>
2023-10-04 02:50:28 +0300

    add node method

--- app_note/lib/cubits/cubit/add_note_cubit.dart
+++ app_note/lib/cubits/cubit/add_note_cubit.dart
@@ -1,6 +1,20 @@
+import 'package:app_note/constant.dart';
 import 'package:app_note/cubits/cubit/add_note_state.dart';
+import 'package:app_note/models/note_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 
 class Notescubit extends Cubit<NotesState> {
   Notescubit(super.initialState);
+
+  addNote(NoteModel note) async {
+    emit(AddNotesLoading());
+    try {
+      var notesbox = Hive.box<NoteModel>(kNotebox);
+      emit(AddNotesSuccess());
+      await notesbox.add(note);
+    } catch (e) {
+      AddNotesFaluir(e.toString());
+    }
+  }
 }
