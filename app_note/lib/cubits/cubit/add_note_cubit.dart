e6596a7827cf1d6c54c07757b73a65814a4d2424
Marwan1231231 <94732913+Marwan1231231@users.noreply.github.com>
2023-10-04 12:31:07 +0300

    add block providers

--- app_note/lib/cubits/cubit/add_note_cubit.dart
+++ app_note/lib/cubits/cubit/add_note_cubit.dart
@@ -5,7 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 class Notescubit extends Cubit<NotesState> {
-  Notescubit(super.initialState);
+  Notescubit() : super(AddNotesInitial());
 
   addNote(NoteModel note) async {
     emit(AddNotesLoading());
--- app_note/lib/main.dart
+++ app_note/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:app_note/Views/note_view.dart';
 import 'package:app_note/constant.dart';
+import 'package:app_note/cubits/cubit/add_note_cubit.dart';
 import 'package:app_note/models/note_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
@@ -16,10 +18,15 @@ class notesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark),
-      home: noteView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => Notescubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark),
+        home: noteView(),
+      ),
     );
   }
 }
